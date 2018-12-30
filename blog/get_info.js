3d5e6792c35cce0fca215da4f91631a6a25a0910
Minh Le <lenguyetminh1998@gmail.com>
2018-12-29 20:25:49 -0500

    Update get_info.js

--- blog/get_info.js
+++ blog/get_info.js
@@ -1,28 +1,23 @@
 // libraries  
-const goodreads = require('goodreads'); 
-const express = require('express')
-const request = require('request'); 
 const $ = require('cheerio'); 
 const rp = require('request-promise');  
+const xml2js = require('xml2js'); 
+const fs = require('fs'); 
 
-// global variables 
-const app = express()
-const port = 3000
-
-// goodreads config
+// Goodreads configuration variables 
 const key = process.env.key; 
-const username = process.env.username; 
 const user_id = process.env.user_id; 
 const shelf_name = 'bilsdungroman'; 
 const shelf_url = 'https://www.goodreads.com/review/list/' + user_id + '?shelf=' +  shelf_name; 
+const parser = new xml2js.Parser(); 
 
-// Functo
+// Parse a .html content (shelf_url)
 function ParseHTMLContent(){
-    rp(shelf_url).then(function(html){
+    rp(shelf_url).then((content) => {
   
-      // Get a list of all links that are within the html page 
+      // Get a list all a tags that are within the shelf_url 
       var links = []; 
-      $('div[class=value] > a', html).each(function () {
+      $('div[class=value] > a', content).each(function () {
           var link = $(this).attr('href');
           links.push(link);
        }); 
@@ -35,32 +30,53 @@ function ParseHTMLContent(){
           return id; 
       }); 
   
-      // Make an API call to get the review 
-      console.log("review_ids", review_ids); 
-      getReviewContent(review_ids); 
-  
-    }).catch(function(err){
-        cosole.log(err); 
+      // Make an API call to get the json parsing of the reviews then send data to Vue fron end 
+      var content_promise_arr = getReviewContent(review_ids); 
+      Promise.all(content_promise_arr).then((values) => {
+        var json = JSON.stringify(values); 
+        fs.writeFile('data.json', json, 'utf8'); 
+      }); 
+    }).catch((err) => {
+        console.log(err) 
     });
 }
-
 
 // Function to get the review content given an url 
+// Return an array of that contain promise objects which if resolved, return the content object 
 function getReviewContent(review_ids){
-    console.log("this function is called!!!"); 
+    var content_promise_arr = []; 
     review_ids.forEach(id => {
+        var p = new Promise((resolve, reject) => {
         var review_url = 'https://www.goodreads.com/review/show.xml?id=' + id + '&key=' + key; 
-        request(review_url, function (error, response, body) {
-            console.log('error:', error); 
-            console.log('statusCode:', response && response.statusCode);
-            console.log("body", body); 
-        }); 
-    });
+        rp(review_url).then((content) => {
+
+            // Parse XML file to JSON
+            parser.parseString(content, function (err, result) {
+
+                // Get book information 
+                var book_obj = result.GoodreadsResponse.review[0].book[0];
+                var book_title = book_obj.title;
+                var book_author = book_obj.authors[0].author[0].name; 
+
+                // Get reviews information 
+                var review_obj = result.GoodreadsResponse.review[0]; 
+                var review_body = review_obj.body[0]; 
+                var rating = review_obj.rating; 
+
+                var content_object = {
+                    'book_title': book_title, 
+                    'book_author': book_author, 
+                    'review_body': review_body, 
+                    'rating': rating 
+                }
+                resolve(content_object); 
+            }); 
+        })
+    }); 
+    content_promise_arr.push(p);       
+    }); 
+    return content_promise_arr; 
 }
 
-
-// Call all functions here 
+// Call functions down here 
 ParseHTMLContent(); 
-
-
-app.listen(port, () =>  console.log(`App listening on port ${port}!`))
